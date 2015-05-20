2ef33ee528061e64779183c7e7cbeb0047aa9f37
jsonZhang <jsonZhang0215@gmail.com>
2015-05-20 23:18:53 +0800

    2.3节完成

--- 《数据结构与算法》/Chapter 2/Array.js
+++ 《数据结构与算法》/Chapter 2/Array.js
@@ -1,5 +1,8 @@
 //数组定义： 一个存储元素的线性集合，元素可以通过索引来任意存取，
 //			索引通常是数字，用来计算元素之间存储位置的偏移量.
+//运行环境： node.js
+
+var readline = require('readline');
 
 //创建数组
 var numbers = [];
@@ -83,5 +86,75 @@ var samenums = [];
 copy(nums, samenums);
 nums[0] = 400;
 console.log(samenums[0]);
+
+//注意：如果数组内的值是一个对象，那么数组中存储的仍然是个引用
+//此时修改任何值仍然会影响所有存储该引用的结果
+var json = {
+	a : 1,
+	b : 2
+}
+var arr1 = [1,json];
+var arr2 = [];
+copy(arr1, arr2);
+//json.b = 3;
+arr2[1].b = 3;
+console.log(arr1, arr2);
+
+//数组存取
+/*var names = ['David', 'Cynthia', 'Raymond', 'Clayton', 'Jennifer'];
+console.log('Enter a name to search for:');
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+rl.question('Please type a name: ', function(name){
+	var pos = names.indexOf(name);
+	if (pos >= 0) {
+		console.log('Found ' + name + ' at position '  + pos);
+	}
+	else {
+		console.log(name + ' not found in array');
+	}
+	rl.close();
+});*/
+
+//使用lastIndexOf方法
+var names = ['David', 'Mike', 'Cynthia', 'Raymond', 'Clayton', 'Mike', 'Jennifer'];
+var name = 'Mike';
+var firstPos = names.indexOf(name);
+var lastPos = names.lastIndexOf(name);
+console.log(firstPos, lastPos);
+
+//数组的字符串表示
+//可使用join()或toString()方法
+var names = ['David', 'Cynthia', 'Raymond', 'Clayton', 'Mike', 'Jennifer'];
+var nameStr = names.join();
+console.log(nameStr);
+nameStr = names.toString();
+console.log(nameStr);
+console.log(names);
+
+//由已有数组创建新数组
+//concat()方法和splice()方法
+//concat()方法 合并数组
+var cisDept = ['Mike', 'Clayton', 'Terrill', 'Danny', 'Jennifer'];
+var dmpDept = ['Raymond', 'Cynthia', 'Bryan'];
+var itDiv = cisDept.concat(dmpDept);
+console.log(itDiv);
+itDiv = dmpDept.concat(cisDept);
+console.log(itDiv);
+
+//splice()方法 拆分数组
+var itDiv = ['Mike', 'Clayton', 'Terrill', 'Raymond', 'Cynthia', 'Danny', 'Jennifer'];
+var dmpDept = itDiv.splice(3,3);
+var cisDept = itDiv;
+console.log(dmpDept);
+console.log(cisDept);
+
+
+
+
+
+
 
 
